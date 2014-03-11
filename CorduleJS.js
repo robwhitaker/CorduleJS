d5b8d5b4e8d4c6726f9f1a2296de7d9af803470e
robwhitaker <Robert.Whitaker1@marist.edu>
2014-03-11 16:47:54 -0400

    Initial commit.

---
+++ CorduleJS.js
@@ -0,0 +1,134 @@
+/**
+ * CorduleJS - a lightweight JavaScript module system
+ * @author Rob Whitaker
+ */
+var CorduleJS = (function() {
+    
+    var modules   = {},
+        observers = {};
+
+    /**
+     * The addModule function adds a module to CorduleJS. If a module by the same name already exists
+     * or the provided module is not an object with init() and destroy() functions, addModule will throw
+     * an error. 
+     * 
+     * @param name      the name of the module to add
+     * @param module    the module object to add
+     */   
+    var addModule = function(name, module) {
+        name = name || '';
+
+        if(typeof module != "object")
+            throw Error("CorduleJS.addModule(). Parameter (module). Module must be an object. Found " + typeof module +".");
+
+        if(modules[name])
+            throw Error("A module by the name " + name + " already exists. Unable to load duplicate module.");
+
+        if(typeof module.init != "function" || typeof module.destroy != "function")
+            throw Error("Integral functions undefined in module '" + name +"': init , destroy. Unable to load module.");
+
+        modules[name] = module;
+
+        modules[name]._ReferralName = name;
+        modules[name].init();
+    } 
+
+    /**
+     * The pushRequest function sends a request and a set of parameters to CorduleJS. If any modules have
+     * observers set for that request, CorduleJS will pass the parameters to the observer's callback function.
+     * This allows modules to interact without having to know about each other.
+     *
+     * @param request    the string holding the name of the request
+     * @param params     the parameter or set of parameters associated with a given request
+     * @return           true if an observer handles the request, false otherwise
+     */ 
+    var pushRequest = function(request, params) {
+        if(!request)
+            return false;
+        
+        if(!params)
+            params = {};
+
+        if(observers[request]) {
+            for(var i = 0; i < observers[request].length; i++)
+                observers[request][i].callback(params);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * The observe function sets up an observer for a given request. This observer is associated with
+     * a specific module, so multiple different modules can have observers listening for the same
+     * request.
+     *
+     * @param moduleRef    a reference to the module that is setting the observer
+     * @param request      the string holding the name of the request to be observed
+     * @param callback     the function to be called when a request is pushed that matches this one
+     * @return             true if observer was successfully set, false otherwise
+     */ 
+    var observe = function(moduleRef,request, callback) {
+        if(!moduleRef || !request || !callback)
+            return false;
+
+        if(observers[request]) {
+
+            for(var i=0; i<observers[request].length; i++)
+                if(observers[request][i].observerModuleName === moduleRef._ReferralName)
+                    return false;
+
+            observers[request].push({"observerModuleName": moduleRef._ReferralName, "callback" : callback});
+        } else
+            observers[request] = [{"observerModuleName": moduleRef._ReferralName, "callback" : callback}];
+
+        return true;
+    }
+
+    /**
+     * The removeObserver function removes an observer from CorduleJS.
+     *
+     * @param moduleRef    a reference to the module that initially set the observer
+     * @param request      the request the observer is listening for
+     * @return             true if the observer has been removed, false otherwise
+     */
+    var removeObserver = function(moduleRef,request) {
+        if(observers[request]) {
+            for(var i = 0; i<observers[request].length; i++) {
+                if(observers[request][i].observerModuleName === moduleRef._ReferralName) {
+                    observers[request].splice(i,1);
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    } 
+
+    /**
+     * The removeModule function calls a modules destroy() function and then deletes
+     * the module from CorduleJS.
+     *
+     * @param moduleName    the name of the module to be removed
+     * @return              true if the module has been removed, false otherwise
+     */
+    var removeModule = function(moduleName) {
+        if(modules[moduleName]) {
+            modules[moduleName].destroy();
+            delete modules[moduleName];
+
+            return true;
+        }
+
+        return false;
+    }
+
+    return {
+        addModule       : addModule,
+        pushRequest     : pushRequest,
+        observe         : observe,
+        removeObserver  : removeObserver,
+        removeModule    : removeModule
+    };
+})();
---
+++ README.md
@@ -0,0 +1,37 @@
+CorduleJS
+==========
+
+CorduleJS (pronounced like "cordial") is short for **Cor**e Mo**dule** **JS**. CorduleJS is a lightweight module control system that streamlines the process of writing modular JavaScript by providing a central hub (or core) which manages all of the modules in an application. While these modules remain unaware of the existence of other modules, CorduleJS enables them to communicate using the `pushRequest` and `observe` functions. 
+
+>####pushRequest
+
+>The `pushRequest` function allows the user to pass a request string and a set of parameters. CorduleJS will attempt to match the request string with an observer that is listening for that type of request. Any observers listening for that request string will fire off their callback function with the set of parameters passed in during the call to `pushRequest`.
+
+>####observe
+
+>The `observe` function creates an observer, or a listener which fires its callback function when the request it is listening for is pushed.
+
+If a request is pushed, but no observers are listening for that request, `pushRequest` will return false, and the program will continue running smoothly. In other words, a missing module should never affect the functionality of other modules. Only the functionality of the missing module will not be present. 
+
+Installation
+--------------
+
+Installation of CorduleJS is as simple as including a single file at the bottom of your `<body>` tags.
+```
+<body>
+<!-- page content -->
+
+<script src="CorduleJS.js"></script>
+<!-- other script includes -->
+</body>
+```
+
+Once CorduleJS is included, you can begin adding modules to your application with the `addModule` function. Just be sure that CorduleJS is loaded before attempting to add modules!
+
+Tutorial
+---------
+*Coming soon.*
+
+License
+-------------
+MIT.
